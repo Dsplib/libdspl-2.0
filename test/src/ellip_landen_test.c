d3471a09c866b1b3924473a3faed1e51c8598339
Dsplib <admin@dsplib.org>
2018-05-25 16:52:16 +0300

    added ellip functions docs

---
+++ test/src/ellip_landen_test.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dspl.h"
+
+#define N 14
+
+int main()
+{
+	void* handle;           // DSPL handle
+	handle = dspl_load();   // Load DSPL function
+	
+	double k[N]; 
+	int i, err;
+	
+	err = ellip_landen(0.93, N, k);
+	if(err != RES_OK)
+	{
+		printf("Error code: %8x\n", err);
+		return err;	
+	}
+
+	printf(" i%8sk[i]\n\n", "");	
+	for(i = 1; i < N; i++)
+		printf("%2d  %11.3e\n", i, k[i]);
+
+	dspl_free(handle);      // free dspl handle
+	return 0;
+}
+
+
