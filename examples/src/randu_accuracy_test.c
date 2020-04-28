22a4dced8a3a829f6a6f0d39321790960c883ffd
Dsplib <admin@dsplib.org>
2020-04-28 21:14:48 +0300

    added doc for randu 	modified:   dspl/dox/ru/randgen.dox 	modified:   examples/src/dspl_info_test.c 	new file:   examples/src/randu_accuracy_test.c 	new file:   examples/src/randu_test.c 	modified:   ide/codeblocks/blas.depend 	modified:   ide/codeblocks/dspl.depend 	modified:   ide/codeblocks/dspl.workspace.layout 	modified:   ide/codeblocks/examples.depend 	modified:   ide/codeblocks/lapack_complex.depend 	modified:   ide/codeblocks/lapack_double.depend

---
+++ examples/src/randu_accuracy_test.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dspl.h"
+
+#define N  10000
+#define L  1.0E-3
+
+
+int randu_accuracy(double* x, int n, double acc, random_t* prnd)
+{
+  double z[512];
+  int cnt = 0;
+  int i;
+  int err = RES_OK;
+  if(!x)
+    return ERROR_PTR;
+  if(n < 1)
+    return ERROR_SIZE;
+  if(acc < 0.0)
+    return ERROR_NEGATIVE;
+  
+  while(cnt < n)
+  {
+    randu(z, 512, prnd);
+    for(i = 0; i < 512; i++)
+    {
+      if((z[i] < acc) && (cnt < n))
+      {
+        x[cnt] = z[i];
+        cnt++;
+      }
+    }
+  }
+  return RES_OK;
+}
+
+
+
+int main(int argc, char* argv[])
+{
+  void* hdspl;            /* DSPL handle        */
+  void* hplot;            /* GNUPLOT handles    */ 
+  random_t rnd = {0};     /* random structure   */
+  hdspl = dspl_load();    /* Load DSPL function */
+  int err;
+  
+  
+  double *x = NULL;
+  double *y = NULL;  
+  
+  x = (double*) malloc(N * sizeof(double));
+  y = (double*) malloc(N * sizeof(double));
+  
+  if(!x || !y)
+  {
+    err = ERROR_PTR;
+    printf("malloc error!\n");
+    goto exit_label;
+  }
+  
+
+  /* MRG32K3A random numbers generator */
+  double seed_mrg32k3a = 1234.0;
+  
+  random_init(&rnd, RAND_TYPE_MRG32K3A, (void*)(&seed_mrg32k3a));
+  randu_accuracy(x, N, L, &rnd); /* accumulate in x numbers from 0 to L */
+  randu_accuracy(y, N, L, &rnd); /* accumulate in y numbers from 0 to L */
+  writetxt(x, y, N, "dat/randu_acc_mrg32k3a.txt");
+  
+
+  /* MT19937 random numbers generator  */
+  unsigned long long seed_mt19937 = 1234353456;
+  random_init(&rnd, RAND_TYPE_MT19937, (void*)(&seed_mt19937));
+  randu_accuracy(x, N, L, &rnd); /* accumulate in x numbers from 0 to L */
+  randu_accuracy(y, N, L, &rnd); /* accumulate in y numbers from 0 to L */
+  writetxt(x, y, N, "dat/randu_acc_mt19937.txt");
+  
+  
+  /* Standard C random numbers generator */
+  randu_accuracy(x, N, L, NULL); /* accumulate in x numbers from 0 to L */
+  randu_accuracy(y, N, L, NULL); /* accumulate in y numbers from 0 to L */
+  writetxt(x, y, N, "dat/randu_acc_std.txt");
+  
+ 
+  /* plotting by GNUPLOT   */
+  /* Create window plot    */
+  err = gnuplot_create(argc, argv, 920, 320, "img/randu_acc_test.png", &hplot);  
+  if(err != RES_OK)
+    goto exit_label;
+  
+  gnuplot_cmd(hplot, "set multiplot layout 1,3 rowsfirst");
+  gnuplot_cmd(hplot, "set xlabel 'x'");
+  gnuplot_cmd(hplot, "set ylabel 'y'");
+  gnuplot_cmd(hplot, "set xtics ('0' 0,'5E-4' 0.0005,' 1E-3' 0.001)");
+  gnuplot_cmd(hplot, "set ytics ('0' 0,'5E-4' 0.0005,' 1E-3' 0.001)");
+  gnuplot_cmd(hplot, "unset key");
+  gnuplot_cmd(hplot, "set title 'MRG32K3A'");
+  gnuplot_cmd(hplot, "plot 'dat/randu_acc_mrg32k3a.txt' with points pointtype 0");
+  gnuplot_cmd(hplot, "set title 'MT19937'");
+  gnuplot_cmd(hplot, "plot 'dat/randu_acc_mt19937.txt'  with points pointtype 0");
+  gnuplot_cmd(hplot, "set title 'Standard C'");
+  gnuplot_cmd(hplot, "plot 'dat/randu_acc_std.txt'  with points pointtype 0");
+  
+exit_label:
+  if(x)
+    free(x);
+  if(y)
+    free(y);
+
+  if(hplot)
+    gnuplot_close(hplot);
+  
+  /* free dspl handle */ 
+  dspl_free(hdspl);
+  return 0;
+}
+
--- examples/src/randu_test.c
+++ examples/src/randu_test.c
@@ -4,128 +4,118 @@
 #include "dspl.h"
 
 #define N  10000
-#define L  1E-3
-
-
-int randu_accuracy(double* x, double* y, int n, double acc, random_t* prnd)
-{
-  double z[512];
-  int err, cnt, i;
-  if(!x || !y)
-    return ERROR_PTR;
-  if(n < 1)
-    return ERROR_SIZE;
-  if(acc < 0)
-    return ERROR_NEGATIVE;
-
-
-  cnt = 0;
-  while(cnt < n)
-  {
-    randu(z, 512, prnd);
-
-	for(i = 0; i < 512; i++)
-	{
-      if(z[i] < acc)
-	  {
-        x[cnt] = z[i];
-		cnt++;
-	  }
-	}
-  }
-
-  cnt = 0;
-  while(cnt < n)
-  {
-    randu(z, 512, prnd);
-	for(i = 0; i < 512; i++)
-	{
-      if(z[i] < acc)
-	  {
-        y[cnt] = z[i];
-		cnt++;
-	  }
-	}
-  }
-
-  return err;
-}
-
 
 int main(int argc, char* argv[])
 {
   void* hdspl;            /* DSPL handle        */
-  void* hplot;            /* GNUPLOT handles    */
+  void* hplot;            /* GNUPLOT handles    */ 
   random_t rnd = {0};     /* random structure   */
   hdspl = dspl_load();    /* Load DSPL function */
-
+  
   double *x = NULL;
-  double *y = NULL;
-
-
+  double *y = NULL;  
+  int err;
+   
   x = (double*) malloc(N * sizeof(double));
   y = (double*) malloc(N * sizeof(double));
-
-
-
+  
+  if(!x || !y)
+  {
+    err = ERROR_PTR;
+    printf("malloc error!\n");
+    goto exit_label;	
+  }
+  
   /***************************************************************************/
   /* MRG32K3A random numbers generator                                       */
   /***************************************************************************/
   double seed_mrg32k3a = 1234.0;
-
-  random_init(&rnd, RAND_TYPE_MRG32K3A, (void*)(&seed_mrg32k3a));
-  randu_accuracy(x, y, N, L, &rnd);
-
-  writetxt(x, y, N, "dat/randu_acc_mrg32k3a.txt");
-
-
-
+  
+  err = random_init(&rnd, RAND_TYPE_MRG32K3A, (void*)(&seed_mrg32k3a));
+  if(err != RES_OK)
+    goto exit_label;
+  
+  err = randu(x, N, &rnd);
+  if(err != RES_OK)
+    goto exit_label;
+  
+  err = randu(y, N, &rnd);
+  if(err != RES_OK)
+    goto exit_label;
+  
+  /* Save to files "dat/randu_mrg32k3a.txt" */
+  writetxt(x, y, N, "dat/randu_mrg32k3a.txt");
+  
+  
+  
   /***************************************************************************/
   /* MT19937 random numbers generator                                        */
   /***************************************************************************/
   unsigned long long seed_mt19937 = 1234353456;
-
-  random_init(&rnd, RAND_TYPE_MT19937, (void*)(&seed_mt19937));
-  randu_accuracy(x, y, N, L, &rnd);
-  writetxt(x, y, N, "dat/randu_acc_mt19937.txt");
-
-
+  
+  err = random_init(&rnd, RAND_TYPE_MT19937, (void*)(&seed_mt19937));
+  if(err != RES_OK)
+    goto exit_label;
+  
+  err = randu(x, N, &rnd);
+  if(err != RES_OK)
+    goto exit_label;
+  
+  err = randu(y, N, &rnd);
+  if(err != RES_OK)
+    goto exit_label;
+  
+  /* Save to files "dat/randu_mrg32k3a.txt" */
+  writetxt(x, y, N, "dat/randu_mt19937.txt");
+  
+  
   /***************************************************************************/
   /* Standard C random numbers generator                                     */
   /***************************************************************************/
-  randu_accuracy(x, y, N, L, NULL);
+  err = randu(x, N, NULL);
+  if(err != RES_OK)
+    goto exit_label;
+  
+  err = randu(y, N, NULL);
+  if(err != RES_OK)
+    goto exit_label;
+
   /* Save to files "dat/randu_std.txt" */
-  writetxt(x, y, N, "dat/randu_acc_std.txt");
-
-
-
+  writetxt(x, y, N, "dat/randu_std.txt");
+  
+  
+  
   /***************************************************************************/
   /* plotting by GNUPLOT                                                     */
   /***************************************************************************/
-  /* Create window plot
-  gnuplot_create(argc, argv, 920, 320,
-                 "img/randu_accuracy_test.png", &hplot);
-  //gnuplot_cmd(hplot, "set grid");
+  /* Create window plot */
+  err = gnuplot_create(argc, argv, 920, 320, "img/randu_test.png", &hplot);  
+  if(err != RES_OK)
+    goto exit_label;
+  
   gnuplot_cmd(hplot, "set multiplot layout 1,3 rowsfirst");
   gnuplot_cmd(hplot, "set xlabel 'x'");
   gnuplot_cmd(hplot, "set ylabel 'y'");
   gnuplot_cmd(hplot, "unset key");
   gnuplot_cmd(hplot, "set title 'MRG32K3A'");
-  gnuplot_cmd(hplot, "plot 'dat/randu_acc_mrg32k3a.txt' with points pointtype 0");
+  gnuplot_cmd(hplot, "plot 'dat/randu_mrg32k3a.txt' with points pointtype 0");
   gnuplot_cmd(hplot, "set title 'MT19937'");
-  gnuplot_cmd(hplot, "plot 'dat/randu_acc_mt19937.txt'  with points pointtype 0");
+  gnuplot_cmd(hplot, "plot 'dat/randu_mt19937.txt'  with points pointtype 0");
   gnuplot_cmd(hplot, "set title 'Standard C'");
-  gnuplot_cmd(hplot, "plot 'dat/randu_acc_std.txt'  with points pointtype 0");
-  gnuplot_close(hplot);
-*/
+  gnuplot_cmd(hplot, "plot 'dat/randu_std.txt'  with points pointtype 0");
+  
+
+
+exit_label:
   if(x)
     free(x);
   if(y)
     free(y);
+  if(hplot)
+    gnuplot_close(hplot); 
 
-
-  /* free dspl handle */
-  dspl_free(hdspl);
+  /* free dspl handle */ 
+  dspl_free(hdspl);      
   return 0;
 }
 
